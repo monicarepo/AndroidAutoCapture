4e7a2d3423611534c4b7f5a1030ce2b12c2718c6
monicarepo <monicaamalanathan@gmail.com>
2025-04-05 09:53:04 -0400

    Added Button

--- app/src/main/java/com/example/autocapture/MainActivity.kt
+++ app/src/main/java/com/example/autocapture/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.autocapture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.autocapture.ui.theme.AutoCaptureTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,10 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        onButtonClick = {
+                            Log.d("MainActivity", "Button clicked!")
+                        }
                     )
                 }
             }
@@ -31,17 +41,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(name: String, modifier: Modifier = Modifier, onButtonClick: () -> Unit) {
+    Column(
         modifier = modifier
-    )
+            .padding(16.dp)
+            .fillMaxWidth(), // You can adjust this based on your layout needs
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = "Welcome to CI/CD",
+            style = MaterialTheme.typography.bodySmall
+        )
+
+        Button(
+            onClick = { onButtonClick() },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Click Me")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AutoCaptureTheme {
-        Greeting("Android")
+        Greeting("Android", onButtonClick = {
+
+        })
     }
 }
